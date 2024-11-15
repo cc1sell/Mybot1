8761261e52c2dbbf9bf1a007c2d012dd074bc3cb
cc1sell <githubccsell@gmail.com>
2024-11-15 17:23:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "hOMhARxQ#nLUTA2wIhw-varHd94c2B1EFCzU95tPGQtC17X7X_sg",
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
 ANTI_LINK: process.env.ANTI_LINK ||"",
 ANTI_BAD_WORDS: process.env.ANTI_BAD_WORDS || "",
